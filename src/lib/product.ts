54453d29dda49cda41745d5c970b1b0e60efeb66
Ryan Ross <ryan@reross.com>
2022-04-25 15:59:12 -0500

    refreshed on classes w/ typescrip

--- src/lib/product.ts
+++ src/lib/product.ts
@@ -1,10 +1,10 @@
 import { Product } from './interfaces';
 
-// This will act as the foundation for other Product type classes (FoodProduct, SportingProduct)
 abstract class ProductBase implements Product {
   constructor(public id: number, public name: string, public icon: string) {}
+
   validate(): boolean {
-    throw new Error('Not implemented');
+    throw new Error('not yet');
   }
 }
 
@@ -13,3 +13,7 @@ export class FoodProduct extends ProductBase {
     return !!this.id && !!this.name && !!this.icon;
   }
 }
+
+let fp = new FoodProduct(1, 'Pizza slice', 'icon.jpg');
+
+class SportingGoodsProduct extends ProductBase {}
